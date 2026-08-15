c48003feacb19c53c58ddf1259f5fea00f85d27f
Ayush Sharma <ayush.sharmaa@hotmail.com>
2026-08-15 23:45:20 +0530

    Commit

--- cloud-loop-app/src/components/providers/ThemeProvider.tsx
+++ cloud-loop-app/src/components/providers/ThemeProvider.tsx
@@ -1,16 +1,20 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { ThemeProvider as DesignThemeProvider } from "@/context/theme/ThemeProvider";
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
   return (
-    <NextThemesProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange={false}
-    >
-      {children}
-    </NextThemesProvider>
+    <DesignThemeProvider>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange={false}
+      >
+        {children}
+      </NextThemesProvider>
+    </DesignThemeProvider>
   );
 }
+
